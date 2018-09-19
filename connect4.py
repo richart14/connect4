d009a67cb83f12962cd7f8df07913b20f9942a0d
Richard Tom <tomrichard14@gmail.com>
2018-09-19 08:20:54 -0700

    inital commit games

---
+++ connect4.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+ROW_COUNT = 6
+COL_COUNT = 7
+
+def create_board():
+  board = np.zeros((ROW_COUNT,COL_COUNT))
+  return board
+
+def drop_piece(board, row, col, piece):
+  board[row][col] = piece
+
+def is_valid_location(board, col):
+  return board[ROW_COUNT-1][col] == 0
+
+def get_next_open_row(board, col):
+  for r in range(ROW_COUNT):
+    if board[r][col] == 0:
+      return r
+
+def print_board(board):
+  print(np.flip(board, 0))
+
+def winning_move(board, piece):
+  # check horizontal locations
+  for c in range(COL_COUNT-3):
+    for r in range(ROW_COUNT):
+      if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+        return True
+
+  # check vertical
+  for c in range(COL_COUNT):
+    for r in range(ROW_COUNT-3):
+      if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+        return True
+  # check for positive slopes
+  for c in range(COL_COUNT-3):
+    for r in range(ROW_COUNT-3):
+      if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+        return True
+  # check for negative slopes
+  for c in range(COL_COUNT-3):
+    for r in range(ROW_COUNT-3, ROW_COUNT):
+      if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+        return True
+
+board = create_board()
+print_board(board)
+game_over = False
+turn = 0 
+
+while not game_over:
+  # ask for player 1 input
+  if turn % 2 == 0:
+    col = int(input("Player 1, Make your selection (0-6):"))
+    print(col)
+
+    if is_valid_location(board, col):
+      row = get_next_open_row(board, col)
+      drop_piece(board, row, col, 1)
+
+      if winning_move(board, 1):
+        print('player 1 wins')
+        game_over = True
+
+  # ask for player 2 input
+  else: 
+    col = int(input("Player 2, Make your selection (0-6):"))
+    print(col)
+    if is_valid_location(board, col):
+      row = get_next_open_row(board, col)
+      drop_piece(board, row, col, 2)
+      if winning_move(board, 2):
+        print('player 2 wins')
+        game_over = True
+  
+  print_board(board)  
+  turn +=1
+  
